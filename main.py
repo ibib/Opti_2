75a1aa10949c143540aa81f02e8c29b398ff43a8
ibib <ibib@gmx.de>
2019-06-17 20:28:09 +0200

    Update main.py
    License correction

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-'''
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <ipeg@august.ovh> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return. Jannes August
- * ----------------------------------------------------------------------------
-'''
+#!/usr/bin/env python3
 
 import os, sys, time, datetime
 import subprocess
